9ff691c06bfaa1b398554a94e4beb3a9199d865c
何江华 <547521847@qq.com>
2019-01-10 22:54:06 +0800

    添加logback

--- rest-support/src/main/java/com/github/ailitech/rest/support/interceptor/HttpIncomeFilter.java
+++ rest-support/src/main/java/com/github/ailitech/rest/support/interceptor/HttpIncomeFilter.java
@@ -1,17 +1,17 @@
 package com.github.ailitech.rest.support.interceptor;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.util.stream.Collectors;
 
 public class HttpIncomeFilter extends HandlerInterceptorAdapter {
+    private static Logger logger= LoggerFactory.getLogger(HttpIncomeFilter.class);
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception{
@@ -21,6 +21,12 @@ public class HttpIncomeFilter extends HandlerInterceptorAdapter {
         }
         String  basePath=request.getContextPath();
         String uri=request.getRequestURI();
+        String uriParam=request.getQueryString();
+        logger.info("request basePath {},uri {},uriparameter {}",basePath,uri,uriParam);
+        if(StringUtils.isNotBlank(httpBody)){
+            logger.info("httpBody: {}",httpBody);
+        }
+
         return true;
     }
 
@@ -32,5 +38,7 @@ public class HttpIncomeFilter extends HandlerInterceptorAdapter {
         if(response instanceof EnhanceHttpServletResponseWrapper){
             httpBody=((EnhanceHttpServletResponseWrapper)response).getResponeData(response.getCharacterEncoding());
         }
+        logger.info("response {}",httpBody);
+        logger.debug("test debug");
     }
 }
